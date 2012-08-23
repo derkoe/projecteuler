226314cc1b7b361744df513963c056ae34b34cb9
derkoe <christian.koeberl@gmail.com>
2012-08-23 13:27:45 +0200

    extracted faculty function

--- euler20.clj
+++ euler20.clj
@@ -1,13 +1,17 @@
-﻿; n! means n × (n - 1) × ... × 3 × 2 × 1
-; For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+; n! means n * (n - 1) * ... * 3 * 2 * 1
+; For example, 10! = 10 * 9 * ... * 3 * 2 * 1 = 3628800,
 ; and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 ; Find the sum of the digits in the number 100!
 
 ; again our digit sum
 (defn digit-sum [n] (reduce + (map #(Character/getNumericValue %) (str n))))
 
+(defn faculty [n] 
+  (reduce *' (range 1 (inc n)))
+)
+
 (def euler20
-  (digit-sum (reduce *' (range 1 101)))
+  (digit-sum (faculty 100))
 )
 
 (println euler20)
