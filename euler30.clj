0fc391452f847d92cdf6459d7a9e4c4ae8cd4e1b
derkoe <christian.koeberl@gmail.com>
2012-08-27 20:11:29 +0200

    problem 30 solved

---
+++ euler30.clj
@@ -0,0 +1,16 @@
+; Project Euler - Problem 30
+; Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+
+(defn exp [n m]
+  (reduce *' (repeat m n))
+)
+
+(defn digits [n] 
+  (map #(Character/getNumericValue %) (str n))
+)
+
+(def euler29
+  (filter (fn [n] (= n (reduce + (map #(exp % 5) (digits n))))) (range 2 354295))
+)
+
+(println (reduce + euler29))
