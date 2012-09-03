ad1970a34a2ccec8c7d73e12c097f33e95e9cf8b
derkoe <christian.koeberl@gmail.com>
2012-09-03 19:49:53 +0200

    a faster factors finishes problem 12

--- euler12.clj
+++ euler12.clj
@@ -1,33 +1,28 @@
 ; http://projecteuler.net/problem=12
 ; What is the value of the first triangle number to have over five hundred divisors?
 
-(defn factors [num]
-  (loop [n 2
-         fset #{1 num}]
-    (if (= (mod num n) 0)
-      (if (contains? fset n)
-        fset
-        (recur (inc n) (conj fset n))
-      )
-      (recur (inc n) fset)
-    )
-  )
-)
+(defn factors [x]
+"integer -> vector[integers]"
+  (loop [xf [] i 2]
+    (if (> (* i i) x)
+      (vec (sort (distinct xf)))
+      (if (zero? (rem x i))
+        (recur (conj xf i (/ x i)) (inc i))
+        (recur xf (inc i))))))
 
 (defn count-factors [num]
   (count (factors num))
 )
 
 (def euler12
-  (loop [num 1000] 
+  (loop [num 29] 
     (let [triangle (reduce + (range 1 num))
           triangle-factors (count-factors triangle)]
-      (do (println triangle triangle-factors)
-      (if (>= triangle-factors 500)
+      (if (> triangle-factors 500)
         triangle
         (recur (inc num))
       )
-    ))
+    )
   )
 )
 
