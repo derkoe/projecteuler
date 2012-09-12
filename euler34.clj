350024d68dda5e5681083c84e585ca41128651d3
derkoe <christian.koeberl@gmail.com>
2012-09-12 20:25:44 +0200

    finished euler 34

--- euler34.clj
+++ euler34.clj
@@ -1,7 +1,7 @@
 ; Project Euler - Problem 34
 ; Find the sum of all numbers which are equal to the sum of the factorial of their digits.
 
-(defn faculty [n] 
+(defn factorial [n] 
   (reduce *' (range 1 (inc n)))
 )
 
@@ -10,11 +10,11 @@
 )
 
 (defn is-curious? [n]
-  (= n (reduce + (map faculty (digits n))))
+  (= n (reduce + (map factorial (digits n))))
 )
 
 (def euler34
-  (filter is-curious? (range 3 10000)) 
+  (reduce + (filter is-curious? (range 3 999999)))
 )
 
 (println euler34)
